5fc9df281ffadea745e4809aae02b45453ed1b68
dannyferc <dannyferc@gmail.com>
2014-09-21 18:22:31 +0200

    Update Q4.R

--- Quizz4/Q4.R
+++ Quizz4/Q4.R
@@ -1,3 +1,11 @@
+#download data.table
+list.of.packages <- c("data.table")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#load data.table
+library(data.table)
+
 dtEd <- data.table(read.csv("./Quizz4/EDSTATS_Country.csv"))
 dt <- merge(dtGDP, dtEd, all=TRUE, by=c("CountryCode"))
 
