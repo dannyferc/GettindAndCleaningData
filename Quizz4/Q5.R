0dc8ffa7b3e085146d83b3b0922f5d46d0218258
dannyferc <dannyferc@gmail.com>
2014-09-21 18:23:06 +0200

    Update Q5.R

--- Quizz4/Q5.R
+++ Quizz4/Q5.R
@@ -1,4 +1,11 @@
+#download data.table
+list.of.packages <- c("quantmod")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+#load quantmod
 library(quantmod)
+
 amzn = getSymbols("AMZN",auto.assign=FALSE)
 sampleTimes = index(amzn) 
 
